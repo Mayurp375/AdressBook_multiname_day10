07e8d9fd33cc24d2cdaa1ad5bcde256cd2cd8656
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-11-06 23:02:01 +0530

    this is adress book multipele name

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        address obj = new address("mayur","polojewar","chamorshi","gadchiroli","44","45463268","hm@gmai");
+        obj.toPrint();
+        address obj2 = new address("sn","pp","chamorshi","gadchiroli","44","45463268","hm@gmai");
+        obj.toPrint();
+
+    }
+}
